6669d21d1b476dd0a21acff478bc6f0ab1fa23ed
ajperlis07 <ajperlis07@gmail.com>
2021-04-12 20:15:14 -0400

    Update on Recipe almost done

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,5 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 function RecipeCard({story, instruction, id, setRecipes}) {
+
+    const [isFormStory, setIsFormStory] = useState(false)
+    const [isFormInstruction, setIsFormInstruction] = useState(false)
+    const [updatedStory, setUpdatedStory] = useState(story)
+    const [updatedInstruction, setUpdatedInstruction] = useState(instruction)
+
+    function handleStoryUpdate(e){
+        e.preventDefault();
+        fetch(`http://localhost:3000/recipes/${id}`,{
+            method: 'PATCH',
+            headers: {"Content-Type": "application/json",
+            accept: "application/json"},
+            body: JSON.stringify({story: updatedStory})
+
+        })
+        setIsFormStory(!isFormStory)
+    }
+
+    function handleInstructionUpdate(e){
+        e.preventDefault();
+        fetch(`http://localhost:3000/recipes/${id}`,{
+            method: 'PATCH',
+            headers: {"Content-Type": "application/json",
+            accept: "application/json"},
+            body: JSON.stringify({instruction: updatedInstruction})
+
+        })
+        setIsFormInstruction(!isFormInstruction)
+    }    
+
+    function handleStoryChange(e){
+        setUpdatedStory(e.target.value)
+    }
+
+    function handleInstructionChange(e){
+        setUpdatedInstruction(e.target.value)
+    }
 
     function handleDelete(e){
         e.preventDefault();
@@ -15,8 +52,24 @@ function RecipeCard({story, instruction, id, setRecipes}) {
 
     return (
         <div>
-            <h2>{instruction}</h2>
-            <p>{story}</p>
+            {isFormInstruction ? (
+                <h2>
+                    <form onSubmit={handleInstructionUpdate}>
+                        <textarea onChange={handleInstructionChange} type="text" value={updatedInstruction} />
+                        <button type="submit">Update Instructions</button>
+                    </form>
+                </h2>
+            ) : (<p onClick={(e) => setIsFormInstruction(!isFormInstruction)}> {updatedInstruction}</p>
+            )}
+             {isFormStory ? (
+                <h2>
+                    <form onSubmit={handleStoryUpdate}>
+                        <textarea onChange={handleStoryChange} type="text" value={updatedStory} />
+                        <button type="submit">Update Story</button>
+                    </form>
+                </h2>
+            ) : (<p onClick={(e) => setIsFormStory(!isFormStory)}> {updatedStory}</p>
+            )}
             <button onClick={handleDelete}>Delete Recipe</button>
         </div>
     )
